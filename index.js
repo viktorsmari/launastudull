aa5c8ee1227925ba33cb999e59d857deab0423fd
Viktor Smári <viktorsmari@users.noreply.github.com>
2017-12-19 11:11:01 +0100

    Add flugvirkjar

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var myapp = new Vue({
     { ratio:1.0,   jobs: 'Verkamenn'},
     { ratio:1.5,   jobs: 'Lögreglumenn?'},
     { ratio:1.6,   jobs: 'Grunnskólakennarar'},
+    { ratio:1.71,  jobs: 'Flugvirkjar 2017'},
     { ratio:1.8,   jobs: 'Læknar'},
     { ratio:4.0,   jobs: 'Alþingismenn'},
     { ratio:7.1,   jobs: 'Ráðherra'},
